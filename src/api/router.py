4b4d68b9ae03a52d8f1f51737a57abb76ab12799
Sesha <seshadrivv28@gmail.com>
2026-03-29 18:56:37 -0400

    added login/register routing for authn

--- src/api/router.py
+++ src/api/router.py
@@ -4,8 +4,10 @@ from fastapi import APIRouter
 
 from src.api.health import router as health_router
 from src.api.web import router as web_router
+from src.auth.router import router as auth_router
 
 api_router = APIRouter()
 
 api_router.include_router(health_router)
+api_router.include_router(auth_router)  # /auth/register, /auth/login, /auth/me
 api_router.include_router(web_router)
